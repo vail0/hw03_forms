c6a1429f0af912438d5b1e5e8c0a9daeacbe1e3b
vail0 <97312541+vail0@users.noreply.github.com>
2022-10-11 21:02:00 +0300

    del files and extra variable

--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -3,7 +3,6 @@ from datetime import date
 
 def year(request):
     """Добавляет переменную с текущим годом."""
-    year = date.today().year
     return {
-        'year': year
+        'year': date.today().year
     }
