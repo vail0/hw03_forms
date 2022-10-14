255b26f7637f77b3f1b98c6337690d1dfa7465a2
vail0 <97312541+vail0@users.noreply.github.com>
2022-10-14 14:23:23 +0300

    test models succeded

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,32 +5,49 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(
+        verbose_name='Название группы',
+        max_length=200,
+    )
+    slug = models.SlugField(
+        verbose_name='Адрес группы',
+        unique=True,
+    )
+    description = models.TextField(
+        verbose_name='Описание группы',
+    )
 
     def __str__(self) -> str:
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(
+        verbose_name='Текст поста',
+        help_text='Графоманить здесь',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
     author = models.ForeignKey(
         User,
+        verbose_name='Автор поста',
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
     )
     group = models.ForeignKey(
         Group,
+        verbose_name='Группа поста',
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
-        related_name='groups'
+        related_name='groups',
+        help_text='Выбери группу, если хочешь',
     )
 
     class Meta:
         ordering = ('-pub_date',)
 
     def __str__(self):
-        return self.text
+        return self.text[:15]
---
+++ yatube/posts/tests/test_models.py
@@ -0,0 +1,97 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from posts.models import Group, Post
+
+User = get_user_model()
+
+# Пост
+
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='auth')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='Тестовый слаг',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый пост',
+        )
+
+    def test_models_have_correct_object_names(self):
+        """Проверяем, что у моделей корректно работает __str__."""
+        # Напишите проверку тут
+        post = PostModelTest.post
+        exp_name = post.text
+        self.assertEqual(exp_name[:15], str(post))
+        pass
+
+    def test_verbose_name(self):
+        """verbose_name в полях совпадает с ожидаемым."""
+        post = PostModelTest.post
+        field_verboses = {
+            'text': 'Текст поста',
+            'pub_date': 'Дата публикации',
+            'author': 'Автор поста',
+            'group': 'Группа поста',
+        }
+        for value, expected in field_verboses.items():
+            with self.subTest(value=value):
+                self.assertEqual(
+                    post._meta.get_field(value).verbose_name, expected)
+
+    def test_help_text(self):
+        """help_text в полях совпадает с ожидаемым."""
+        post = PostModelTest.post
+        field_help_texts = {
+            'text': 'Графоманить здесь',
+            'group': 'Выбери группу, если хочешь',
+        }
+        for value, expected in field_help_texts.items():
+            with self.subTest(value=value):
+                self.assertEqual(
+                    post._meta.get_field(value).help_text, expected)
+
+
+# Группа
+
+
+class GroupModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='auth')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='Тестовый слаг',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый пост',
+        )
+
+    def test_models_have_correct_object_names(self):
+        """Проверяем, что у моделей корректно работает __str__."""
+        # Напишите проверку тут
+        group = GroupModelTest.group
+        exp_name = group.title
+        self.assertEqual(exp_name, str(group))
+        pass
+
+    def test_verbose_name(self):
+        """verbose_name в полях совпадает с ожидаемым."""
+        group = GroupModelTest.group
+        field_verboses = {
+            'title': 'Название группы',
+            'slug': 'Адрес группы',
+            'description': 'Описание группы',
+        }
+        for value, expected in field_verboses.items():
+            with self.subTest(value=value):
+                self.assertEqual(
+                    group._meta.get_field(value).verbose_name, expected)
